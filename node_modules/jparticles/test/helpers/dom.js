e271faa13cfec583dd8010bfa99660e735776928
requireS <solaiman@students.uni-marburg.de>
2018-11-04 17:02:22 +0100

    Optimize nodemodules and package.json

--- node_modules/jparticles/test/helpers/dom.js
+++
@@ -1,5 +0,0 @@
-import jsdom, {JSDOM} from 'jsdom';
-const dom = new JSDOM('<!DOCTYPE html>');
-global.window = dom.window;
-global.document = dom.window.document;
-global.navigator = dom.window.navigator;
