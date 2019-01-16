9856b6603bc6a74e377b2f29c0518dba029516e5
requireSol <Solaiman@students.uni-marburg.de>
2019-01-16 03:39:02 +0100

    Optimize Dropdown: toogle on windowclick

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,52 +1,44 @@
+var count = 0;
 
- 
-/**NAVBAR FUNCTIONALITYS*/
-
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-/*function dropDownToogle() {
-    document.getElementById("myDropdown").classList.toggle("show");
-    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
-		$(this).toggleClass('open');
-	});
-    //$(".dropbtn").click(function(){
-    //  $("dropdown-content").fadeIn()
-   // });
-  }*/
-  
-  // Close the dropdown if the user clicks outside of it
-  
-  $(document).ready(function(){
-	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
-      document.getElementById("myDropdown").setAttribute("class", "show"); 
-      $(this).toggleClass('open');
-	});
-});
+function toogleDropdown(){
+  console.log("sdfsdf");
+  document.getElementById("myDropdown").classList.toggle("show");
+  $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').toggleClass('open');
+  count = 0;
+}
 
 
+// Get the modal
+var modal = document.getElementById('signin');
+var modal2 = document.getElementById('signup');
 
-   // Get the modal
-   var modal = document.getElementById('signin');
-   var modal2 = document.getElementById('signup');
-   
-   // When the user clicks anywhere outside of the modal or dropdown, close it
-   window.onclick = function(event) {
-       if (event.target == modal) {
-           modal.style.display = "none";
-       }else if(event.target == modal2){
-         modal2.style.display = "none";
-   }
+// When the user clicks anywhere outside of the modal or dropdown, close it
+var modal = document.getElementById('signin');
+var modal2 = document.getElementById('signup');
 
-     if (!(event.target.matches('.dropdown'))) {}
-         if (($( "#myDropdown" ).hasClass( "show" ))) {
-           document.getElementById("myDropdown").setAttribute("class" , "show");
-           $(this).toggleClass('open');
-           //console.log("gg");
-         }else{
-          document.getElementById("myDropdown").setAttribute("class" , "dropdown-content");
-          $(this).toggleClass('open');
-         }
-   }
- 
-
-    
+// When the user clicks anywhere outside of the modal or dropdown, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }else if(event.target == modal2){
+      modal2.style.display = "none";
+     }
+    if (!event.target.matches('.dropdown')) {
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+  var i;
+  //klappt wenn bisschen an classen getrickst wird
+  for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+      //das darf nicht das erste mal aufgerufen weden da es das gezeigte direkt versteckt
+      if(count != 0){
+      openDropdown.classList.remove('show');
+      $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').toggleClass('open');
+      }
+      count++;
+      //$(this).toggleClass('open');
+      //toogleDropdown();
+    }
+  }
+}
+}
