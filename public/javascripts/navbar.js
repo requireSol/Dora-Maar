ac4992053bf8410ba8d774f0aa5fba916772fe38
requireSol <Solaiman@students.uni-marburg.de>
2019-01-16 02:17:28 +0100

    fork datas

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,4 +1,5 @@
 
+ 
 /**NAVBAR FUNCTIONALITYS*/
 
 /* When the user clicks on the button, 
@@ -15,3 +16,37 @@ toggle between hiding and showing the dropdown content */
   
   // Close the dropdown if the user clicks outside of it
   
+  $(document).ready(function(){
+	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+      document.getElementById("myDropdown").setAttribute("class", "show"); 
+      $(this).toggleClass('open');
+	});
+});
+
+
+
+   // Get the modal
+   var modal = document.getElementById('signin');
+   var modal2 = document.getElementById('signup');
+   
+   // When the user clicks anywhere outside of the modal or dropdown, close it
+   window.onclick = function(event) {
+       if (event.target == modal) {
+           modal.style.display = "none";
+       }else if(event.target == modal2){
+         modal2.style.display = "none";
+   }
+
+     if (!(event.target.matches('.dropdown'))) {}
+         if (($( "#myDropdown" ).hasClass( "show" ))) {
+           document.getElementById("myDropdown").setAttribute("class" , "show");
+           $(this).toggleClass('open');
+           //console.log("gg");
+         }else{
+          document.getElementById("myDropdown").setAttribute("class" , "dropdown-content");
+          $(this).toggleClass('open');
+         }
+   }
+ 
+
+    
